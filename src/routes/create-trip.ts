5c5ffe06a4653a3a20a9a3c3d7f8235421add4e2
gerson aguiar <gersoncafilho@gmail.com>
2024-07-09 14:56:50 -0300

    create post route

---
+++ src/routes/create-trip.ts
@@ -0,0 +1,7 @@
+import type { FastifyInstance } from "fastify";
+
+export async function createTrip(app: FastifyInstance){
+    app.post('/trips', async () => {
+        return 'Heelo World!'
+    })
+}
