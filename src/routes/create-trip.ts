c139e5dd28136e2ecf259735a0c54fd961c74d4c
gerson aguiar <gersoncafilho@gmail.com>
2024-07-09 15:53:14 -0300

    validated start and end date

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
+import { prisma } from "../lib/prisma";
+import dayjs from "dayjs";
 
 export async function createTrip(app: FastifyInstance){
     app.withTypeProvider<ZodTypeProvider>().post('/trips', {
@@ -14,10 +16,22 @@ export async function createTrip(app: FastifyInstance){
     }, async (request) => {
         const { destination, starts_at, ends_at } = request.body
         
-        return {
-            destination,
-            starts_at,
-            ends_at
+        if(dayjs(starts_at).isBefore(new Date())){
+            throw new Error("Start date must be in the future")
         }
+
+        if(dayjs(ends_at).isBefore(dayjs(starts_at))){
+            throw new Error("End date must be after start date")
+        }
+
+        const trip = await prisma.trip.create({
+            data: {
+                destination,
+                starts_at,
+                ends_at
+            }
+        })
+
+        return {tripId: trip.id        }
     })
 }
