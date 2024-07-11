acb678c4d667ab1a91dee7b5e43b9c5e49b06642
gerssonmg <gersoncafilho@gmail.com>
2024-07-10 22:35:29 -0300

    aula03 - error custom and finish endPoints

--- src/routes/create-activity.ts
+++ src/routes/create-activity.ts
@@ -3,6 +3,7 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
 import { prisma } from "../lib/prisma";
 import { dayjs } from '../lib/dayjs';
+import { ClientError } from "../errors/client-error";
 
 export async function createActivity(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post(
@@ -26,15 +27,15 @@ export async function createActivity(app: FastifyInstance) {
             })
 
             if (!trip) {
-                throw new Error('Trip not found')
+                throw new ClientError('Trip not found')
             }
 
             if (dayjs(occurs_at).isBefore(dayjs(trip.starts_at))) {
-                throw new Error('Activity date is before trip start date')
+                throw new ClientError('Activity date is before trip start date')
             }
 
             if (dayjs(occurs_at).isAfter(dayjs(trip.ends_at))) {
-                throw new Error('Invalid activity date.')
+                throw new ClientError('Invalid activity date.')
             }
 
             const activity = await prisma.activity.create({
--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,9 @@ import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
 import { createInvite } from "./routes/create-invite";
 import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
+import { errorHandler } from "./error-handler";
 
 const app = fastity()
 
@@ -22,6 +25,8 @@ app.register(cors, {
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+app.setErrorHandler(errorHandler)
+
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
@@ -32,6 +37,9 @@ app.register(getLinks)
 app.register(getParticipants)
 app.register(createInvite)
 app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
+
 
 
 
